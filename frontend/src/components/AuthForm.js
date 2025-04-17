08d176c9d9b72aeda6eda84ad5a2b3ecdf619c34
e.karchevsky@mail.ru <e.karchevsky@mail.ru>
2025-04-17 20:41:59 +0400

    remove registration

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -3,16 +3,11 @@ import axios from 'axios';
 import './AuthForm.css';
 
 const AuthForm = ({ onLogin }) => {
-    const [isRegistering, setIsRegistering] = useState(false);
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
-    const [telegramUsername, setTelegramUsername] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
-    const initialUsers = [
-        { login: 'admin', password: 'admin', role: 'admin', telegramUsername: '@Werzant' },
-        { login: 'asd', password: 'asd', role: 'help', telegramUsername: '@user' },
-    ];
+
     const handleCheck = async (login, password) => {
         try {
             const response = await axios.post(`http://localhost:8000/transactions/verdict/`, [login, password]);
@@ -36,52 +31,26 @@ const AuthForm = ({ onLogin }) => {
         }
     };
 
-    const handleRegistration = async (login, password, tgid) => {
-        try {
-            const response = await axios.post(`http://localhost:8000/transactions/verdict/`, [login, password, tgid]);
-            handleGetAnswer()
-        } catch (error) {
-            console.error('Error creating item:', error);
-        }
-    };
-
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setErrorMessage('');
 
         if (!login || !password) {
-            setErrorMessage('Пожалуйста, заполните все обязательные поля.');
+            setErrorMessage('Пожалуйста, заполните все поля.');
             return;
         }
 
-        if (isRegistering) {
-            if (!telegramUsername) {
-                setErrorMessage('Пожалуйста, укажите имя пользователя Telegram для регистрации.');
-                return;
-            }
 
-            console.log('Регистрация:', { login, password, telegramUsername });
-            handleRegistration(login, password, telegramUsername);
-            setIsRegistering(false);
-            setErrorMessage('Регистрация прошла успешно. Войдите, используя свой логин и пароль.');
+        handleCheck(login, password)
 
-            return;
-        } else {
-            handleCheck(login,password)
-        }
     };
 
-
-    const toggleFormMode = () => {
-        setIsRegistering(!isRegistering);
-        setErrorMessage('');
-    };
 
     return (
         <div className="auth-form-container">
             <div className="auth-form">
-                <h2>{isRegistering ? 'Регистрация' : 'Авторизация'}</h2>
+                <h2>Авторизация</h2>
                 {errorMessage && <div className="error-message">{errorMessage}</div>}
                 <form onSubmit={handleSubmit}>
                     <div className="form-group">
@@ -106,26 +75,10 @@ const AuthForm = ({ onLogin }) => {
                             className="form-control"
                         />
                     </div>
-                    {isRegistering && (
-                        <div className="form-group">
-                            <label htmlFor="telegram">Telegram Username:</label>
-                            <input
-                                type="text"
-                                id="telegram"
-                                value={telegramUsername}
-                                onChange={(e) => setTelegramUsername(e.target.value)}
-                                required
-                                className="form-control"
-                            />
-                        </div>
-                    )}
                     <button type="submit" className="btn btn-primary">
-                        {isRegistering ? 'Зарегистрироваться' : 'Войти'}
+                        Войти
                     </button>
                 </form>
-                <button className="btn btn-secondary toggle-btn" onClick={toggleFormMode}>
-                    {isRegistering ? 'Уже есть аккаунт? Войти' : 'Нет аккаунта? Зарегистрироваться'}
-                </button>
             </div>
         </div>
     );
